6c1b7eae16519611b6652985e96c0e093139fd1d
oa00179 <oa00179@srcs.org>
2019-01-16 10:09:49 -0800

    oof

--- app/src/main/java/com/example/bruce/snake_startercode/SnakeSegment.java
+++ app/src/main/java/com/example/bruce/snake_startercode/SnakeSegment.java
@@ -1,4 +1,58 @@
+/*************************************************
+ * By: Olivia Armstrong
+ *
+ * Code for Snake Segment
+ *
+ * 1/11/19
+ **************************************************/
 package com.example.bruce.snake_startercode;
 
 public class SnakeSegment {
+    private int mXLoc;
+    private int mYLoc;
+    private int mLengthOfSnake;
+    private Body mBodyParts;
+    private int mDegree;
+
+
+    /*************************************************
+     * Enumerations
+     **************************************************/
+
+    public enum Body {
+
+        HEAD, BODY, TAIL;
+    }
+
+    /*************************************************
+     * Getters and Setters
+     **************************************************/
+
+    public int getmXLoc() {
+        return mXLoc;
+    }
+
+    public int getmYLoc() {
+        return mYLoc;
+    }
+
+    public int getmLengthOfSnake() {
+        return mLengthOfSnake;
+    }
+
+    public void setmLengthOfSnake(int mLengthOfSnake) {
+        this.mLengthOfSnake = mLengthOfSnake;
+    }
+
+    public Body getmBodyParts() {
+        return mBodyParts;
+    }
+
+    public int getmDegree() {
+        return mDegree;
+    }
+
+    public void setmDegree(int mDegree) {
+        this.mDegree = mDegree;
+    }
 }
