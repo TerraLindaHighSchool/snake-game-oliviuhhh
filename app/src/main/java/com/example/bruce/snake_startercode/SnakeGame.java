83afd7fe46801008a9015c1734ffd37b861974da
oa00179 <oa00179@srcs.org>
2019-02-04 13:36:43 -0800

    lab 4

--- app/src/main/java/com/example/bruce/snake_startercode/SnakeGame.java
+++ app/src/main/java/com/example/bruce/snake_startercode/SnakeGame.java
@@ -39,19 +39,59 @@ public class SnakeGame {
 
   }
 
-  protected boolean play(){
-    for(int segment = 0; segment < mSnake.size(); segment++){
-      mSnake.get(segment).setXLoc(mSnake.get(segment).getXLoc()+1);
+  protected boolean play() {
+    SnakeSegment.Body seg;
+    int xLoc, yLoc, degrees;
 
-      if(mSnake.get(segment).getXLoc() > mXMax){
-        mGameOver = true;
+    // increments through segments
+    for (int i = 0; i < mSnake.size(); i++) {
+      seg = mSnake.get(i).getmBodyParts();
+      xLoc = mSnake.get(i).getXLoc();
+      yLoc = mSnake.get(i).getYLoc();
+      degrees = mSnake.get(i).getmDegrees();
+
+
+      // checks if segment is on pivot
+      for (int p = 0; p < mPivotPoint.size(); p++) {
+        int xPivot = mPivotPoint.get(p).getXCoord();
+        int yPivot = mPivotPoint.get(p).getYCoord();
+        int degPivot = mPivotPoint.get(p).getDegree();
+
+        // is segment i at a pivot point
+        if (mSnake.get(i).getXLoc() == mPivotPoint.get(p).getXCoord() && mSnake.get(i).getYLoc() == mPivotPoint.get(p).getYCoord()) {
+
+          // code to turn segment
+          if (xLoc == mPivotPoint.get(p).getXCoord() && yLoc == mPivotPoint.get(p).getYCoord()) {
+            mSnake.get(i).setmDegrees(mPivotPoint.get(p).getDegree());
+            if (mSnake.get(mSnake.size() - 1).getXLoc() == mPivotPoint.get(p).getXCoord() && mSnake.get(mSnake.size() - 1).getYLoc() == mPivotPoint.get(p).getYCoord()) {
+              mPivotPoint.remove(p);
+            }
+          }
+        }
       }
-    }
 
-    return mGameOver;
-  }
+      // move a segment in increments
+      switch (degrees) {
+        case 180:
+          mSnake.get(i).setXLoc(--xLoc);
+          break;
 
-  private void setAppleCoord(){
+        case 90:
+          mSnake.get(i).setYLoc(++yLoc);
+          break;
+
+        case 0:
+          mSnake.get(i).setXLoc(++xLoc);
+          break;
+
+        case 270:
+          mSnake.get(i).setYLoc(--yLoc);
+      }
+
+
+
+
+      private void setAppleCoord(){
     mAppleCoord[0] = (int)(mBOARD_WIDTH*Math.random());
     mAppleCoord[1] = (int)(mBOARD_HEIGHT*Math.random());
   }
